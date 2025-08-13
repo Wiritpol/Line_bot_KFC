836ef76d4b12a0e832b041cd6b7fc633cace56a3
Wiritpol <156336174+Wiritpol@users.noreply.github.com>
2025-08-13 09:44:51 +0700

    Update webhook_kfc.py
    update privacy

--- webhook_kfc.py
+++ webhook_kfc.py
@@ -12,8 +12,8 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 # Your LINE Channel Secret and Access Token
-CHANNEL_SECRET = '6fa3fae3a93a0fa521e4f1e6779e4499'
-CHANNEL_ACCESS_TOKEN = 'jxO7jKpnn4jjvbp1/gnwJW6Ps0Gf40B6Rc7ljl14ll6VjBdBBOgl8TRWqVKJ3gixmvJgT1iOAWRjdww3wHrdVDByBYiUMIO67uSuZqxdK4TZ2N+gymKnpTLkjOUlzAAYe8N75PRJq0/+I5osNm1E6AdB04t89/1O/w1cDnyilFU='
+CHANNEL_SECRET = 'xxx'
+CHANNEL_ACCESS_TOKEN = 'xxx'
 
 
 
@@ -123,4 +123,5 @@ def handle_message(event):
 
 if __name__ == "__main__":
     app.run(port=5000)
+
 
